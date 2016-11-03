ab92e2577776a8ac444beb4c42d952b1f9900994
alexb <alexb@10.0.1.142>
2016-11-03 15:05:08 +0100

    Primo commit

---
+++ 02 Iniezione Degli Oggetti/src/main/java/it/alfasoft/alex/iniezioneDegliOggetti/Esempio.java
@@ -0,0 +1,19 @@
+package it.alfasoft.alex.iniezioneDegliOggetti;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Esempio {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+
+		Triangolo t = (Triangolo) context.getBean("triangolo");
+
+		t.stampaVertici();
+
+	}
+
+}
